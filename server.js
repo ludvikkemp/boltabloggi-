0a821610586055ffb7a7be8213b1317337e64c6f
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-04-19 01:08:48 +0000

    mongoDB connection estabilesed

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// Database Configuration
+const dbConnectionString = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(dbConnectionString)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello"));
 

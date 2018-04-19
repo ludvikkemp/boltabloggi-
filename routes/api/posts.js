99ed0cca11ddc860836629aaa79b8d0fe998106d
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-04-19 01:25:21 +0000

    routes/api created 2

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// @route   GET api/posts/test
+// @descr   Tests post route
+// @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Users Posts Works' }));
 
 module.exports = router;

faacc6456fd831842a99c3416b1d999ec6a88f98
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-05-25 23:51:15 +0000

    UserShcema created and exported

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
-users = require("./routes/api/users");
-profile = require("./routes/api/profile");
-posts = require("./routes/api/posts");
+users = require('./routes/api/users');
+profile = require('./routes/api/profile');
+posts = require('./routes/api/posts');
 
 const app = express();
 
 // Database Configuration
-const dbConnectionString = require("./config/keys").mongoURI;
+const dbConnectionString = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
   .connect(dbConnectionString)
-  .then(() => console.log("MongoDB Connected"))
+  .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello"));
+app.get('/', (req, res) => res.send('Hello'));
 
 // User Routes
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 

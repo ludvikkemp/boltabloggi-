adc83449dec35ff2f641b78e23530a9ee358e78d
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-06-30 14:53:07 +0000

    jwt auth implementation

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 users = require('./routes/api/users');
 profile = require('./routes/api/profile');
@@ -21,7 +22,11 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello'));
+// Passport Middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 // User Routes
 app.use('/api/users', users);
